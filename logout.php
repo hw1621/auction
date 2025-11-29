a686a0a262960a1558cf915fc9bf87b2a13b7df0
Cecilia0328 <Cecilia0328@outlook.com>
2025-11-29 01:01:10 +0000

    Simplify logout process and change redirect target
    Refactor logout logic to use session_unset and redirect to browse.php.

--- logout.php
+++ logout.php
@@ -1,14 +1,10 @@
 <?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 
-session_start();
-
-unset($_SESSION['logged_in']);
-unset($_SESSION['account_type']);
-setcookie(session_name(), "", time() - 360);
+session_unset();
 session_destroy();
 
-
-// Redirect to index
-header("Location: index.php");
-
-?>
+header("Location: browse.php");
+exit;
